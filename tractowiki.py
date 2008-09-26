4379991193a5f27b63a8b33f42de33e1b79d1ebc
bestchai <>
2008-09-25 22:43:00 -0400

    1

--- tractowiki.py
+++ tractowiki.py
@@ -1,8 +1,7 @@
 import pickle
-import datetime
 from subprocess import call
 import os
-githubID = {'albert': "aaaaalbert", 'justinc': "JustinCappos", 'sebastien': 'awwad', 'leonwlaw': 'linkleonard', 'choksi81': "choksi81", 'us341':"us341", 'ivan': "bestchai", 'armon': "armon"}
+githubID = {'albert': "aaaaalbert <>", 'justinc': "JustinCappos <>", 'sebastien': 'awwad <>', 'leonwlaw': 'linkleonard <>', 'choksi81': "choksi81<>", 'us341':"us341 <>", 'ivan': "bestchai <>", 'armon': "armon <>"}
 wiki_records_list = pickle.load(file("trac_wiki_dump.pickle"))
 #how will I know that the UNIX time sort works?
 wiki_records_list.sort(key = lambda row: row[2])
@@ -11,7 +10,7 @@ for i in wiki_records_list:
   #print str(i) + "\n"
   name = i[0].encode('utf8')
   version = i[1]
-  time = datetime.datetime.fromtimestamp(i[2]).strftime('%Y-%m-%d %H:%M:%S')
+  time = i[2]
   #time = i[2]
   author = i[3]
   text = i[4].encode('utf8')
@@ -23,13 +22,13 @@ for i in wiki_records_list:
       if not os.path.isdir(name):
         continue
 
-  f = open(str(name),"w")
+  f = open(str(name) + str(version) + ".wiki", "w")
   f.write(text)
   f.close()
   
-  # call(["git","add", "."])
-  # call(["git", "commit", "-m","\"" + str(version)+str(comment) + "\"", "--date= ","\"" +  str(time) + "\"", "--author=", "\"" + str(githubID[author]) + "\""])
-  # call(["git", "push", "origin","master"])
+  call(["git","add", "."])
+  call(["git", "commit", "-m" + str(version) + str(comment), "--date=" + str(time), "--author=" + str(githubID[author])])
+  call(["git", "push", "origin","master"])
     # Moin Moin Wiki is a python  and Ivan
 
   
