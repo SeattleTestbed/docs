e9c0deb08cd3042c7a9b48d13d082c54b6e5d177
Priyam <pn613@nyu.edu>
2016-03-02 18:31:59 -0500

    Updated code

--- tractowiki.py
+++ tractowiki.py
@@ -1,7 +1,8 @@
 import pickle
+import datetime
 from subprocess import call
 import os
-#githubID = {'albert': "aaaaalbert", 'justinc': "JustinCappos", ‘sebastien’: “awwad”, ‘leonwlaw’: “linkleonard”, 'choksi81': "choksi81", 'us341':"us341"}
+githubID = {'albert': "aaaaalbert", 'justinc': "JustinCappos", 'sebastien': 'awwad', 'leonwlaw': 'linkleonard', 'choksi81': "choksi81", 'us341':"us341", 'ivan': "priyam3nidhi", 'armon': "priyam3nidhi"}
 wiki_records_list = pickle.load(file("trac_wiki_dump.pickle"))
 #how will I know that the UNIX time sort works?
 wiki_records_list.sort(key = lambda row: row[2])
@@ -10,7 +11,8 @@ for i in wiki_records_list:
   #print str(i) + "\n"
   name = i[0].encode('utf8')
   version = i[1]
-  time = i[2]
+  time = datetime.datetime.fromtimestamp(i[2]).strftime('%Y-%m-%d %H:%M:%S')
+  #time = i[2]
   author = i[3]
   text = i[4].encode('utf8')
   comment = i[5]
@@ -24,10 +26,11 @@ for i in wiki_records_list:
   f = open(str(name),"w")
   f.write(text)
   f.close()
-  #call(["git","init"])
-  #call(["git","add", "."])
-  #call(["git", "commit", "-a", "-m", """ + str(version)+str(comment) + """, "--date= ", time, "--author=", githubID[author])
-    # Moin Moin Wiki is a python extension
+  
+  call(["git","add", "."])
+  call(["git", "commit", "-m","\"" + str(version)+str(comment) + "\"", "--date= ","\"" +  str(time) + "\"", "--author=", "\"" + str(githubID[author]) + "\""])
+  call(["git", "push", "origin","master"])
+    # Moin Moin Wiki is a python  and Ivan
 
   
 
